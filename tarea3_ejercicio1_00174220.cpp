cc5d119073bb5581bc4b9fbe8e275161beb60115
GerardoAmaya <62672927+GerardoAmaya@users.noreply.github.com>
2020-05-09 17:48:48 -0600

    Add files via upload

---
+++ tarea3_ejercicio1_00174220.cpp
@@ -0,0 +1,26 @@
+//Realizar un programa en c++ que devuelva el promedio de 3 números enteros ingresados por el usuario.
+
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+
+float num1, num2, num3, resultado;
+
+printf("Hola, ingresa tu primer numero");
+scanf("%f", &num1);
+
+printf("Ingrese su segundo numero");
+scanf("%f", &num2);
+
+printf("Ingrese su tercer numero");
+scanf("%f", &num3);
+
+resultado=(num1+num2+num3)/3;
+printf("El promedio es %f",resultado);
+
+ 
+ return 0;
+
+}
